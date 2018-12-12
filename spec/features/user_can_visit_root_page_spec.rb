d5d54e99cc787a6e5072efe78b929b0a43354acd
Emilio <thecraftedgem@tuta.io>
2018-12-12 09:16:15 -0700

    feature test almost done

--- spec/features/user_can_visit_root_page_spec.rb
+++ spec/features/user_can_visit_root_page_spec.rb
@@ -8,4 +8,19 @@ feature "User can visit root page" do
       expect(page).to have_selector("input[value='Search by zip...']")
     end
   end
+
+  it "can do something" do 
+  
+    visit "/"
+    
+    fill_in search form with: 80203 (Note: Use the existing search form)
+    click_on "Locate"
+
+    expect(current_path).to be("/search")
+    expect(page).to have_content("Here are the 10 closest stations within 6 miles")
+  end
 end
+
+
+# And the stations should be limited to Electric and Propane
+# And for each of the stations I should see Name, Address, Fuel Types, Distance, and Access Times```
