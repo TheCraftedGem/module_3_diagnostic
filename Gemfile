573b1016905f7f7b597ff70338cc750a50144483
Emilio <thecraftedgem@tuta.io>
2018-12-12 10:33:22 -0700

    finally back on track

--- Gemfile
+++ Gemfile
@@ -15,6 +15,8 @@ gem 'figaro'
 group :development, :test do
   gem 'byebug'
   gem 'rspec-rails'
+  gem 'pry'
+  gem 'launchy'
 end
 
 group :development do
--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,13 +1,15 @@
 class SearchController < ApplicationController
   def index
-    zip = params[:zip]
-    @conn = Faraday.new(url: "https://developer.nrel.gov/api") do |faraday|
-      faraday.headers["api_key"] = "y8aUxWBstojUPi0gCjpcKT9On6Sak8nvV1BABUTX"
+  
+    zip = params[:q]
+    @conn = Faraday.new(url: "https://developer.nrel.gov") do |faraday|
+      faraday.headers['X-Api-Key'] = ENV["api_key"]
       faraday.adapter Faraday.default_adapter
+     
     end
-
-    response = @conn.get("alt-fuel-stations/v1.json")
+    response = @conn.get("api/alt-fuel-stations/v1.json")
   
     @stations = JSON.parse(response.body, symbolize_names: true)
+    binding.pry
   end
 end
